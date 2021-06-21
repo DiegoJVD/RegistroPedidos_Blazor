11d8489df30939cc40f0eda164c16a24ff903c95
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-20 23:18:11 -0400

    Creando contexto

---
+++ RegistroPedidos_Blazor/DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegistroPedidos_Blazor.Models;
+
+namespace RegistroPedidos_Blazor.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Ordenes> Ordenes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Suplidores> Suplidores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite(@"Data Source=Data\RegistroPedidosBlazor.db");
+        }
+    }
+}
