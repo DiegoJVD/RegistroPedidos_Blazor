79c17189423c83b8192482fcefb0121548542c38
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-21 14:02:28 -0400

    agregando datos en el OnModelCreating

--- RegistroPedidos_Blazor/DAL/Contexto.cs
+++ RegistroPedidos_Blazor/DAL/Contexto.cs
@@ -19,5 +19,17 @@ namespace RegistroPedidos_Blazor.DAL
 
             optionsBuilder.UseSqlite(@"Data Source=Data\RegistroPedidosBlazor.db");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Suplidores>().HasData(
+                new Suplidores { SuplidorId = 1, Nombres = "Enel Almonte" }
+                );
+
+            modelBuilder.Entity<Productos>().HasData(
+                new Productos { ProductoId = 1, Descripcion = "Audifonos", Costo = 500, Inventario = 100 }
+                );
+        }
     }
 }
--- RegistroPedidos_Blazor/Models/Ordenes.cs
+++ RegistroPedidos_Blazor/Models/Ordenes.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroPedidos_Blazor.Models
 {
     public class Ordenes
     {
+        [Key]
         public int OrdenId { get; set; }
 
         public DateTime Fecha {get; set; }
@@ -17,5 +19,8 @@ namespace RegistroPedidos_Blazor.Models
 
         [Range(minimum: 0.0, maximum: 10000000.0, ErrorMessage = "Inserte un monto válido")]
         public Decimal Monto { get; set; }
+
+        [ForeignKey("OrdenId")]
+        public List<OrdenesDetalle> Detalle { get; set; } = new List<OrdenesDetalle>();
     }
 }
